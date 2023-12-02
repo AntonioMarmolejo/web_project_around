bdf1db7d211443fd3acde3a2d7cabfebdd5c5642
Antonio Marmolejo <djcamg71@gmail.com>
2023-12-02 11:25:28 -0500

    style: se aplican comentarios a las funciones del en el scrips especificando lo que hacen

--- scripts.js
+++ scripts.js
@@ -1,11 +1,28 @@
+//Se selecciona el bloque donde donde va la foto de perfil, el nombre, la actividad y el boton de editar perfil
 let botonPincelEditar = document.querySelector(".profile__intro_edit-button");
+
+//Se selecciona el elemento de la X para cerrar el formulario
 let inconoCerrarFomulario = document.querySelector(".edit-form_icono");
+
+//Se selecciona el bloque donde está todo el formulario incluyendo el boton X de cerrar
 let todoElformulario = document.querySelector(".edit-form");
+
+//Se selecciona el bloque donde va el nombre del usuario
 let nombreUsuario = document.querySelector(".profile__intro_name-explorer");
+
+//Se selecciona el bloque dode va la actividad que realiza la el usuario
 let actividadUsuario = document.querySelector(".profile__intro_explorer");
+
+//Se selecciona el identificador del formulario que es el input donde el usuario ingresara su nombre
 let entradaNombre = document.querySelector("#primer-nombre");
+
+//Se selecciona el identificado del formulario que es el input donde el usuario ingresará su actividad
 let entradaActividad = document.querySelector("#actividad");
+
+//Se selecciona el bloque que nos permite resaltar el formulario oscureciendo el fondo
 let overlayVentana = document.querySelector(".overlay");
+
+//Se selecciona el botón que guardará nuestros datos y pondrá los datos ingresado en los bloques correspondientes
 let botonGuadar = document.querySelector(".form__submi");
 
 //Función para cerrar el formulario y desactivar el overlay para esclarecer la página
@@ -24,7 +41,8 @@ function abrirFormulario() {
 }
 botonPincelEditar.addEventListener("click", abrirFormulario);
 
-//Función para almacenar los datos ingresados por el usuario y remplazarlos por los que ya estan por defecto al momento de darle click al boton de guardar.
+//Función para almacenar los datos ingresados por el usuario y remplazarlos por
+//los que ya estan por defecto al momento de darle click al boton de guardar.
 function almacenarDatos(event) {
   event.preventDefault();
 
